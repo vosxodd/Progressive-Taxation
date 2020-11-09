41b227302e927311828687b908f9c7c6bb651c8b
vosxodd <73089079+vosxodd@users.noreply.github.com>
2020-11-09 06:21:19 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,19 +2,19 @@ status=input()
 revenue=int(input())
 if status=="родитель-одиночка":
     if 0<=revenue<=12950:
-        print(0.1*revenue)
+        tax = 0.1 * revenue
     elif 12951<=revenue<=49400:
-        print(0.15*revenue)
+        tax = 0.15 * revenue
     elif 49401<=revenue<=127550:
-        print(0.25*revenue)
+        tax = 0.25 * revenue
     elif 127551<=revenue<=206600:
-        print(0.28*revenue)
+        tax = 0.28 * revenue
     elif 206601<=revenue<=405100:
-        print(0.33*revenue)
+        tax = 0.33 * revenue
     elif 405101<=revenue<=432200:
-        print(0.35*revenue)
+        tax = 0.35 * revenue
     elif 432201<=revenue:
-        print(0.396*revenue)
+        tax = 0.396 * revenue
         
        
 if status=="один субъект":
@@ -36,16 +36,16 @@ if status=="один субъект":
         
 if status == 'для супружеской пары':
     if 0<=revenue<=18150:
-        print(0.1*revenue)
+        tax = 0.1 * revenue
     elif 18151<=revenue<=73800:
-        print(0.15*revenue)
+        tax = 0.15 * revenue
     elif 73801<=revenue<=148850:
-        print(0.25*revenue)
+        tax = 0.25 * revenue
     elif 148851<=revenue<=226850:
-        print(0.28*revenue)
+        tax = 0.28 * revenue
     elif 226851<=revenue<=405100:
-        print(0.33*revenue)
+        tax = 0.33 * revenue
     elif 405101<=revenue<=457600:
-        print(0.35*revenue)
+        tax = 0.35 * revenue
     elif 457601<=revenue:
-        print(0.396*revenue)
+        tax = 0.396 * revenue
