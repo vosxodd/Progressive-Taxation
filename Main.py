0d0502b1a391f2b40208945a27f35a4fb5ac0753
vosxodd <73089079+vosxodd@users.noreply.github.com>
2020-11-12 13:53:09 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -66,19 +66,44 @@ for i in range (0,12):
 
         
         
-if status == 'для супружеской пары':
-    if 0<=revenue<=18150:
-        tax = 0.1 * revenue
-    elif 18151<=revenue<=73800:
-        tax = 0.15 * revenue
-    elif 73801<=revenue<=148850:
-        tax = 0.25 * revenue
-    elif 148851<=revenue<=226850:
-        tax = 0.28 * revenue
-    elif 226851<=revenue<=405100:
-        tax = 0.33 * revenue
-    elif 405101<=revenue<=457600:
-        tax = 0.35 * revenue
-    elif 457601<=revenue:
-        tax = 0.396 * revenue
+for i in range(0, 12):
+
+    if status == "для супружеской пары":
+        if 0 <= a[i] - taxfree <= 18150:
+            tax = tax + (a[i] - taxfree) * 0.1
+        elif 18151 <= a[i] - taxfree <= 73800:
+            tax = tax + (a[i] - 9076 - taxfree) * 0.15
+            tax = tax + (18150) * 0.1
+        elif 73801 <= a[i] - taxfree <= 148850:
+            tax += 0.25 * (a[i] - 73801 - taxfree)
+            tax += (73800 - 18151) * 0.15
+            tax += (18150) * 0.1
+        elif 148851 <= a[i] - taxfree <= 226850:
+            tax += 0.28 * (a[i] - 148851 - taxfree)
+            tax += 0.25 * (148850 - 73801)
+            tax += (73800 - 18151) * 0.15
+            tax += (18150) * 0.1
+        elif 226851 <= a[i] - taxfree <= 405100:
+            tax += 0.33 * (a[i] - 226851 - taxfree)
+            tax += 0.28 * (226850 - 148851)
+            tax += 0.25 * (148850 - 73801)
+            tax += (73800 - 18151) * 0.15
+            tax += (18150) * 0.1
+        elif 405101 <= a[i] - taxfree <= 457600:
+            tax += 0.35 * (a[i] - 405101 - taxfree)
+            tax += 0.33 * (405100 - 226851)
+            tax += 0.28 * (226850 - 148851)
+            tax += 0.25 * (148850 - 73801)
+            tax += (73800 - 18151) * 0.15
+            tax += (18150) * 0.1
+        elif 457601 <= a[i] - taxfree:
+            tax += 0.396 * (a[i] - 406751 - taxfree)
+            tax += 0.35 * (406750 - 405101)
+            tax += 0.33 * (405100 - 226851)
+            tax += 0.28 * (226850 - 148851)
+            tax += 0.25 * (148850 - 73801)
+            tax += (73800 - 18151) * 0.15
+            tax += (18150) * 0.1
+
+
 print(tax)
